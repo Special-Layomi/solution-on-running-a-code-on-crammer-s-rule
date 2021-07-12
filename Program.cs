1298d7b7cb5c7c838a2a791527211b8eaf6a0905
Special-Layomi <87333581+Special-Layomi@users.noreply.github.com>
2021-07-12 19:38:55 +0100

    Add files via upload
    Application is for solving a problem using crammer's rule through c#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace layomi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          float a,b,c,d,e,f;
+            Console.WriteLine("Enter a:");
+            a = Convert.ToSingle(Console.ReadLine());
+            System.Console.WriteLine("Enter b:");
+            b = Convert.ToSingle(Console.ReadLine());
+            System.Console.WriteLine("Enter c:");
+            c = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter d:");
+            d = Convert.ToSingle(Console.ReadLine());
+            System.Console.WriteLine("Enter e:");
+            e = Convert.ToSingle(Console.ReadLine());
+            System.Console.WriteLine("Enter f:");
+            f = Convert.ToSingle(Console.ReadLine());
+            float determinant = (a*d)-(b*c);
+            float changeInX = (e*d) - (f*b);
+            float changeInY = (a*f) - (e*c);
+            if (determinant == 0)
+            {
+                System.Console.WriteLine("The equation has no solution"); 
+            }
+            else{
+                float x = changeInX/determinant;
+                float y = changeInY/determinant;
+                Console.WriteLine("The value of \n X is {0} \n Y is {1}", x,y);
+            }
+
+        }
+    }
+}
